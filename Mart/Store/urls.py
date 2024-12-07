2f40336b07d1a2a669ef5ca2d014b108e58a3ff2
Raju <drzraju@gamil.com>
2024-12-07 21:04:27 +0600

    add create new store view

--- Mart/Store/urls.py
+++ Mart/Store/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
+    path("", views.createStore , name=""),
     path('<str:slug>/', views.store , name='store' )
 ]
--- Mart/Store/views.py
+++ Mart/Store/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import *
+from rest_framework import status
+from rest_framework.response import Response
 from .serializers import storeSerializers
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-
+@csrf_exempt
 def store(request,slug):
     stores = Store.objects.filter(slug = slug)
     serializer = storeSerializers(stores,many = True)
     return JsonResponse(serializer.data,safe =  False)
+
+def createStore(request):
+    if request.method == 'POST':
+            serializer = storeSerializers(data = request.data)
+            if serializer.is_valid():
+                serializer.save()
+                
+                return Response(serializer.data,status = status.HTTP_201_CREATED)  
+            return Response (serializer.errors,status= status.HTTP_400_BAD_REQUEST)
