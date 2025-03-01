5d4ad6ef4fcf1e9fdee434613dce8e89dc3b4e06
Raju <drzraju@gamil.com>
2025-03-01 22:34:21 +0600

    create sellers model

--- mart/Sellers/models.py
+++ mart/Sellers/models.py
@@ -1,3 +1,18 @@
+
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    full_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    zip = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.email
