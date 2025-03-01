def6b8b9ce6c87a4b6e48369ad4c21c94c736f8b
Raju <drzraju@gamil.com>
2025-03-02 00:22:17 +0600

    setup urls for selller api

--- mart/mart/urls.py
+++ mart/mart/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from Sellers.views import*
+
+from rest_framework.routers import DefaultRouter
+routers = DefaultRouter()
+routers.register(r"sellers", SellerViewSet,basename="seller")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('user/',include(routers.urls))
 ]
