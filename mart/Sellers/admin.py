38dc80079d1b0ece7c9ff58ff7e64a16c0117834
Raju <drzraju@gamil.com>
2025-03-02 00:20:38 +0600

    create sellers viewset

--- mart/Sellers/admin.py
+++ mart/Sellers/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+class sellerAdmin(admin.ModelAdmin):
+    readonly_fields = ("password",)
+admin.site.register(User,sellerAdmin)
--- mart/Sellers/views.py
+++ mart/Sellers/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
+from .serializers import SellerSerializer
+from .models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework import viewsets
+from django.contrib.auth.hashers import make_password
+
 # Create your views here.
+
+class SellerViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = SellerSerializer
+    
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        
+        password = data.get("password")
+        confirm_password = data.get("confirm_password",None)
+        
+        if password != confirm_password :
+            return Response({'error': "Password missmatch"},status= status.HTTP_308_PERMANENT_REDIRECT)
+        
+        if User.objects.filter(email=data.get("email")).exists():
+            return Response({'error': "Email already exist"},status= status.HTTP_207_MULTI_STATUS)
+        
+        seriallizer = self.get_serializer(data=data)
+        
+        if seriallizer.is_valid(raise_exception=True):
+            seriallizer.save( password = make_password(password))
+        
+            return Response({'message': "User registration success"},status= status.HTTP_201_CREATED)
+    
