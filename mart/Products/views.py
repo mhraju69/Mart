cb734ca34d1131aabc81aaf541e6743109cd69ca
Raju <drzraju@gamil.com>
2025-03-02 12:54:28 +0600

    create products viewsets and update urls

--- mart/Products/views.py
+++ mart/Products/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .serializers import ProductSerializer
+from .models import Products
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+class productViewset(viewsets.ModelViewSet):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- mart/mart/urls.py
+++ mart/mart/urls.py
@@ -3,16 +3,18 @@ from django.contrib import admin
 from django.urls import path,include
 from Sellers.views import*
 from Store.views import*
+from Products.views import*
 from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.conf.urls.static import static
 routers = DefaultRouter()
 routers.register(r"sellers", SellerViewSet,basename="seller")
 routers.register(r"store", storeViewset ,basename="store")
+routers.register(r"product", productViewset ,basename="product")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/',include(routers.urls))
+    path('api/',include(routers.urls))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
