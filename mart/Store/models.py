bc2ce28b50bcb3f45a23550c8fbe0328ac06162b
Raju <drzraju@gamil.com>
2025-03-07 14:18:22 +0600

    update store  model

--- mart/Store/models.py
+++ mart/Store/models.py
@@ -3,7 +3,7 @@ from Sellers.models import User
 # Create your models here.
 
 class Store(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'is_seller': True})
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     address = models.TextField()
