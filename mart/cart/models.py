0b969ea3a5a0b00e4f8d938ec56cd1018c3b0be9
Raju <drzraju@gamil.com>
2025-03-17 21:35:33 +0600

    add cart and cart items model

---
+++ mart/cart/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from Sellers.models import User
+from Products.models import *
+
+# Create your models here.
+
+class Cart(models.model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    
+    
+    
