08ee32178c68f73c3ee858d848004ad902668a85
Raju <drzraju@gamil.com>
2025-03-21 11:17:15 +0600

    update cart and cart viewset for buyer only

--- mart/cart/models.py
+++ mart/cart/models.py
@@ -6,11 +6,16 @@ from Products.models import *
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.email
+    
     
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
+    def __str__(self):
+        return f"{self.product.name} {self.cart}"
     
     
     
--- mart/cart/views.py
+++ mart/cart/views.py
@@ -11,12 +11,18 @@ class CartViewset(viewsets.ModelViewSet):
     lookup_field = 'product_slug'
     serializer_class = CartSerializer
     def get_cartitems(self,request):
-        cart = Cart.objects.get(user=request.user)
+        user=request.user
+        if not user.is_buyer :
+            return Response({'error':'You are not a buyer'},status=status.HTTP_400_BAD_REQUEST)
+        cart = get_object_or_404(Cart,user=user)
         serializer = CartSerializer(cart)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response({'user':user.email ,"Cart" :serializer.data},status=status.HTTP_200_OK)
 
     def create(self,request,product_slug=None,*args, **kwargs):
         user = request.user
+        if not user.is_buyer :
+            return Response({'error':'user must be a buyer'},status=status.HTTP_400_BAD_REQUEST)
+            
         cart , created = Cart.objects.get_or_create(user=user)
         product = get_object_or_404(Products,product_slug=product_slug)
         cartitem , created = CartItem.objects.get_or_create(cart=cart ,product=product)
